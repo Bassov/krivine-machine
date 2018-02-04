b73c0545e23172e2a63e07df100367c6d4e889e1
Alexandr Basov <bainst4@gmail.com>
2018-02-04 17:04:48 +0300

    Remove usage of Stack
    Instead of using Stack, it is possible to use built-in List

--- src/KrivineMachine.hs
+++ src/KrivineMachine.hs
@@ -2,153 +2,86 @@ module KrivineMachine where
 
 import LambdaCompiler
 
-data Stack a
-  = EmptyStack
-  | Push a
-         (Stack a)
-  deriving (Show)
-
-push :: a -> Stack a -> Stack a
-push = Push
-
-top :: Stack a -> a
-top (Push x _) = x
-
-pop :: Stack a -> Stack a
-pop (Push _ xs) = xs
-
-emptyStack :: Stack a
-emptyStack = EmptyStack
-
-isEmpty :: Stack a -> Bool
-isEmpty xs =
-  case xs of
-    EmptyStack -> True
-    _ -> False
-
 data Closure =
   Closure CTerm
-          (Stack (Stack Closure))
+          [[Closure]]
   deriving (Show)
 
-data None =
-  None
-
-krivine :: Stack Closure -> Either (Int, Closure) (Stack Closure)
-krivine (Push (Closure t e) s) =
+krivine :: [Closure] -> Either (Int, Closure) [Closure]
+krivine (Closure t e:s) =
   case t of
-    Constant c -> Right (Push (Closure (Constant c) EmptyStack) s)
+    Constant c -> Right $ Closure (Constant c) [] : s
     CVariable nu i ->
       case subs nu i e of
-        Right cl -> Right (Push cl s)
-        Left None -> Right (Push (Closure (CVariable nu i) EmptyStack) s)
-    CApplication v u -> Right (Push (Closure v e) (Push (Closure u e) s))
+        Just cl -> Right $ cl : s
+        Nothing -> Right $ Closure (CVariable nu i) [] : s
+    CApplication v u -> Right $ Closure v e : Closure u e : s
     Lambda i v ->
       case pops i s of
-        (0, xs') -> Right (Push (Closure v (Push (pushs i 1 s) e)) xs')
-        (k, _) -> Left (k, Closure v (Push (pushs i 1 s) e))
+        (0, xs') -> Right $ Closure v (pushs i 1 s : e) : xs'
+        (k, _)   -> Left (k, Closure v (pushs i 1 s : e))
 
-pops :: Int -> Stack a -> (Int, Stack a)
-pops i xs =
-  case i of
-    0 -> (0, xs)
-    _ ->
-      case xs of
-        Push _ xs' -> pops (i - 1) xs'
-        _ -> (i, EmptyStack)
+pops :: Int -> [a] -> (Int, [a])
+pops 0 xs     = (0, xs)
+pops i []     = (i, [])
+pops i (_:xs) = pops (i - 1) xs
 
-pushs :: (Num a, Eq a) => a -> Int -> Stack Closure -> Stack Closure
-pushs i k xs =
-  case i of
-    0 -> EmptyStack
-    _ ->
-      case xs of
-        Push x xs' -> Push x (pushs (i - 1) k xs')
-        _ ->
-          Push
-            (Closure (CVariable 1 k) EmptyStack)
-            (pushs (i - 1) (k + 1) EmptyStack)
+pushs :: Int -> Int -> [Closure] -> [Closure]
+pushs 0 _ _      = []
+pushs i k []     = Closure (CVariable 1 k) [] : pushs (i - 1) (k + 1) []
+pushs i k (x:xs) = x : pushs (i - 1) k xs
 
-subs :: Int -> Int -> Stack (Stack Closure) -> Either None Closure
-subs nu i xs =
-  case nu of
-    1 ->
-      case xs of
-        Push s _ -> sub i s
-        _ -> Left None
-    _ ->
-      case xs of
-        Push _ xs' -> subs (nu - 1) i xs'
-        _ -> Left None
+subs :: Int -> Int -> [[Closure]] -> Maybe Closure
+subs _ _ []      = Nothing
+subs 1 i (x:_)   = sub i x
+subs nu i (_:xs) = subs (nu - 1) i xs
 
-sub :: Int -> Stack Closure -> Either None Closure
-sub i xs =
-  case i of
-    1 ->
-      case xs of
-        Push c _ -> Right c
-        _ -> Left None
-    _ ->
-      case xs of
-        Push _ xs' -> sub (i - 1) xs'
-        _ -> Left None
+sub :: Int -> [Closure] -> Maybe Closure
+sub _ []     = Nothing
+sub 1 (x:_)  = Just x
+sub i (_:xs) = sub (i - 1) xs
 
-completeEnvironment :: Int -> Stack Closure -> Stack Closure
-completeEnvironment j xs =
-  case j of
-    0 -> xs
-    _ ->
-      Push (Closure (CVariable 1 j) EmptyStack) (completeEnvironment (j - 1) xs)
+completeEnvironment :: Int -> [Closure] -> [Closure]
+completeEnvironment 0 xs = xs
+completeEnvironment j xs = Closure (CVariable 1 j) [] : completeEnvironment (j - 1) xs
 
-close :: CTerm -> Stack Closure
-close t = Push (Closure t EmptyStack) EmptyStack
+close :: CTerm -> [Closure]
+close t = [Closure t []]
 
-currentTerm :: Stack Closure -> CTerm
-currentTerm xs =
-  case xs of
-    Push c _ ->
-      case c of
-        Closure t _ -> t
+currentTerm :: [Closure] -> CTerm
+currentTerm (Closure t _: _) = t
 
-state :: String -> Either String (Stack Closure)
+state :: String -> Either String [Closure]
 state s =
   case compiledTerm s of
     Right t -> Right (close t)
     Left cs -> Left cs
 
-environment :: Stack Closure -> Stack (Stack Closure)
-environment xs =
-  case xs of
-    Push c _ ->
-      case c of
-        Closure _ e -> e
+environment :: [Closure] -> [[Closure]]
+environment (Closure _ e : _) = e
 
-krivineMachine :: Stack Closure -> CTerm
+krivineMachine :: [Closure] -> CTerm
 krivineMachine xs =
   case krivine xs of
-    Right (Push cl xs') ->
+    Right (cl:xs') ->
       case cl of
         Closure (Constant c) _ -> apply (Constant c) xs'
         Closure (CVariable nu i) e ->
           case e of
-            EmptyStack ->
+            [] ->
               case xs' of
-                EmptyStack -> CVariable nu i
-                _ -> apply (CVariable nu i) xs'
-            _ -> krivineMachine (Push cl xs')
-        Closure (Lambda _ _) _ -> krivineMachine (Push cl xs')
-        _ -> krivineMachine (Push cl xs')
-    Left (k, cl) -> Lambda k (krivineMachine (Push cl EmptyStack))
+                [] -> CVariable nu i
+                _  -> apply (CVariable nu i) xs'
+            _ -> krivineMachine (cl:xs')
+        Closure (Lambda _ _) _ -> krivineMachine (cl:xs')
+        _ -> krivineMachine (cl:xs')
+    Left (k, cl) -> Lambda k (krivineMachine [cl])
 
-apply :: CTerm -> Stack Closure -> CTerm
-apply t xs =
-  case xs of
-    EmptyStack -> t
-    Push c xs' ->
-      apply (CApplication t (krivineMachine (Push c EmptyStack))) xs'
+apply :: CTerm -> [Closure] -> CTerm
+apply = foldl (\ t x -> CApplication t (krivineMachine [x]))
 
 computeCTerm :: CTerm -> CTerm
-computeCTerm t = krivineMachine (close t)
+computeCTerm = krivineMachine . close
 
 compute :: String -> String
 compute s =
@@ -157,11 +90,6 @@ compute s =
       case state s of
         Right t -> convertCTermToString (krivineMachine t)
     Left cs -> cs
-
-environmentState :: Stack Closure -> Stack (Stack Closure)
-environmentState s =
-  case top s of
-    Closure _ e -> e
 
 run :: IO String
 run = getLine >>= \x -> return (compute x)
